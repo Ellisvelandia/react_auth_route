021d9d188ee999bb6200440792daf170fc076a26
ellisvelandia <eyis619@gmail.com>
2022-08-08 03:04:25 -0500

    Bear

---
+++ src/Components/bear/Bear.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../bear/Bear.scss';
+
+const Bear = () => {
+  return (
+    <div id="container">
+      <div class="head"></div>
+      <div class="muzzle"></div>
+      <div class="ears"></div>
+      <div class="body"></div>
+      <div class="paws-front"></div>
+      <div class="paws-back"></div>
+      <div class="details"></div>
+      <div class="tail"></div>
+
+      <div class="sleep">
+        <span class="s1">z</span>
+        <span class="s2">z</span>
+        <span class="s3">z</span>
+        <span class="s4">z</span>
+        <span class="s5">z</span>
+      </div>
+    </div>
+  );
+};
+
+export default Bear;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,54 @@
-
+import { Link } from 'react-router-dom';
+import './Styled/Dashboard.scss';
+import Bear from '../Components/bear/Bear';
 
 const Dashboard = ({ user }) => {
   return (
-    <section className='section'>
-      <h4>Hello, {user?.name}</h4>
-  
+    <section className="section-dash">
+      <h4 className="user">Hello, {user?.name}</h4>
+      <div className="Dash">
+        <input type="checkbox" id="check" />
+        <label for="check">
+          <i className="fas fa-bars" id="sidebar_btn"></i>
+        </label>
+
+        <header className="Head">
+          <div className="right_area">
+            <Link to="/" className="logout_btn">
+              Back Home ?
+            </Link>
+          </div>
+        </header>
+
+        <div className="sidebar">
+          <center>
+            <img
+              src="https://e7.pngegg.com/pngimages/734/25/png-clipart-waving-bear-bear-brown-bear.png"
+              className="profile_image"
+            />
+            <h4>Bear</h4>
+          </center>
+          <a href="#">
+            <i className="fas fa-desktop"></i>
+            <span>Dashboard</span>
+          </a>
+          <a href="#">
+            <i className="fas fa-cog"></i>
+            <span>Componets</span>
+          </a>
+          <a href="#">
+            <i className="fas fa-table"></i>
+            <span>Tables</span>
+          </a>
+          <a href="#">
+            <i className="fas fa-sliders-h"></i>
+            <span>Settings</span>
+          </a>
+        </div>
+        <div className="Content">
+          <Bear />
+        </div>
+      </div>
     </section>
   );
 };
