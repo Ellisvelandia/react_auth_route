1d0105ff7604db294b4c491e560b093611372cbd
ellisvelandia <eyis619@gmail.com>
2022-08-07 21:33:58 -0500

    ProtectedRoute

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,12 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Styled/Error.scss';
 
 const Error = () => {
   return (
     <section className="section">
-      <h2>404</h2>
-      <p>Page not found</p>
-      <Link to="/">Back Home</Link>
+      <div class="mars"></div>
+      <img src="https://assets.codepen.io/1538474/404.svg" class="logo-404" />
+      <img src="https://assets.codepen.io/1538474/meteor.svg" class="meteor" />
+      <p class="title">Oh no!!</p>
+      <p class="subtitle">
+        You’re either misspelling the URL <br /> or requesting a page that's no
+        longer here.
+      </p>
+      <div align="center"></div>
+      <img
+        src="https://assets.codepen.io/1538474/astronaut.svg"
+        class="astronaut"
+      />
+      <img
+        src="https://assets.codepen.io/1538474/spaceship.svg"
+        class="spaceship"
+      />
+      <Link to="/" class="btn-back">
+        Back to previous page
+      </Link>
     </section>
   );
 };
